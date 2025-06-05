792eefde14867151ecb26bd126c629dbe92f2ff0
Nicholas Condon <40219252+NickCondon@users.noreply.github.com>
2025-06-05 22:59:55 +1000

    Create build.bat

---
+++ build_scripts/build.bat
@@ -0,0 +1,17 @@
+@echo off
+echo Building ReMInD executables...
+
+echo Installing requirements...
+pip install -r requirements_build.txt
+
+echo Building Full Version...
+pyinstaller --onefile --windowed --name "ReMInD_v2.26" --add-data "../src/metadata_extractors;metadata_extractors" ../src/ReMInD_v2.26.py
+
+echo Building Lite Version...
+pyinstaller --onefile --windowed --name "ReMInD_Lite_v2.26" --add-data "../src/metadata_extractors;metadata_extractors" ../src/ReMInD_Lite_v2.26.py
+
+echo Moving executables to dist folder...
+move dist\*.exe ..\dist\
+
+echo Build complete!
+pause
